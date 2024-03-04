6eadd200c973c3f71362298f3ef15ca00669d75d
Mickael Turquais <mickael.turquais@laposte.net>
2024-03-04 01:13:19 +0100

    feat: animation de la bombe

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -8,43 +8,71 @@ import { Colors, Colors8 } from '../colors';
 import SecretCode, { generateRandomCode } from '../secretCode';
 import Modal from '../modal';
 import AnimationBomb from '../animationBomb';
+import Pause from '../pause';
 
 export default function Board({ level }) {
 
+    // États du composant
     const [rows, setRows] = useState(Array.from({ length: 8 }, () => ({ colors: ['', '', '', ''], exactMatches: 0, misplacedMatches: 0, remainingDotsLength: 4 })));
     const [activeRowIndex, setActiveRowIndex] = useState(0);
     const [selectedPawnIndex, setSelectedPawnIndex] = useState(0);
     const [gameOver, setGameOver] = useState(false);
     const [newCode, setNewCode] = useState(generateRandomCode(level));
     const [modalOpen, setModalOpen] = useState(false);
+    const [messageOpen, setMessageOpen] = useState(false);
+    const [isPaused, setIsPaused] = useState(false);
 
+    // Fonctions de gestion des modales
+    const handleOpenMessage = () => {
+        setMessageOpen(true);
+        setIsPaused(true);
+    };
+
+    const handleCloseMessage = () => {
+        setMessageOpen(false);
+    };
+
+    // Fonctions de gestion des modales
     const handleOpenModal = () => {
         setModalOpen(true);
-      };
+    };
     
-      const handleCloseModal = () => {
+    const handleCloseModal = () => {
         setModalOpen(false);
-      };
+    };
 
+    const toggleAnimation = () => {
+        setIsPaused(true); // Met en pause l'animation
+    };
+
+    // Fonction pour redémarrer l'animation
+    const restartAnimation = () => {
+        setIsPaused(false); // Redémarre l'animation
+    };
+
+    // Détermination de la palette de couleurs en fonction du niveau
     const colorsCode = level === 'Difficile' ? Colors8 : Colors;
 
+    // Génération d'un nouveau code secret
     const handleGenerateCode = () => {
         const newCode = generateRandomCode(level);
         setNewCode(newCode);
-        console.log("board" + newCode)
+        console.log(newCode)
     };
     
-
+    // Vérification de la rangée
     const checkRow = useCallback((rowColors) => {
         let exactMatches = 0;
         let misplacedMatches = 0;
 
+        // Comparaison des couleurs avec le code secret
         for (let i = 0; i < rowColors.length; i++) {
             if (rowColors[i] === newCode[i]) {
                 exactMatches++;
             }
         }
 
+        // Décompte des couleurs correctes mais mal placées
         const colorsCount = {};
         const newCodeCount = {};
 
@@ -64,66 +92,73 @@ export default function Board({ level }) {
 
         misplacedMatches -= exactMatches;
 
+        // Calcul de la longueur des points restants
         const remainingDotsLength = 4 - exactMatches - misplacedMatches;
 
         return { exactMatches, misplacedMatches, remainingDotsLength };
     }, [newCode]);
 
-    const handleCheck = useCallback(() => {
-        if (!gameOver && activeRowIndex !== -1) {
-            const currentRow = rows[activeRowIndex];
-    
-            const isRowComplete = currentRow.colors.every(color => color !== '');
-    
-            if (isRowComplete) {
-                let { exactMatches, misplacedMatches, remainingDotsLength } = checkRow(currentRow.colors);
-                const newRows = [...rows];
-                newRows[activeRowIndex] = { ...currentRow, exactMatches, misplacedMatches, remainingDotsLength };
-    
-                // Adjust behavior based on the selected level
-                if (level === 'Facile') {
-                    // If beginner level, move correct colors to the next row
-                    if (activeRowIndex < rows.length - 1) {
-                        // Vérifiez si ce n'est pas la dernière ligne
-                        const newRow = { ...newRows[activeRowIndex + 1] };
-                        // Vérifiez s'il n'y a aucune couleur trouvée sur la ligne actuelle
-                        const foundColors = newRows[activeRowIndex].colors.filter((color, index) => color === newCode[index]);
-                        const noFoundColors = foundColors.length === 0;
-                        if (!noFoundColors) {
-                            // Vérifiez si la ligne suivante ne contient pas déjà de couleurs trouvées
-                            const nextRowFoundColors = newRows[activeRowIndex + 1].colors.filter(color => color !== '');
-                            const nextRowNoFoundColors = nextRowFoundColors.length === 0;
-                            if (nextRowNoFoundColors) {
-                                // Copiez les couleurs trouvées de la ligne actuelle sur la ligne suivante
-                                for (let i = 0; i < currentRow.colors.length; i++) {
-                                    if (currentRow.colors[i] === newCode[i]) {
-                                        newRow.colors[i] = currentRow.colors[i];
-                                    }
-                                }
-                                newRows[activeRowIndex + 1] = newRow;
-                            }
+// Fonction pour vérifier la rangée actuelle
+const handleCheck = useCallback(() => {
+    if (!gameOver && activeRowIndex !== -1) {
+        const currentRow = rows[activeRowIndex];
+
+        // Vérifie si la rangée est complète
+        const isRowComplete = currentRow.colors.every(color => color !== '');
+
+        if (isRowComplete) {
+            let { exactMatches, misplacedMatches, remainingDotsLength } = checkRow(currentRow.colors);
+            const newRows = [...rows];
+            newRows[activeRowIndex] = { ...currentRow, exactMatches, misplacedMatches, remainingDotsLength };
+
+            // Comportement différent selon le niveau de difficulté (Facile)
+            if (level === 'Facile') {
+                if (activeRowIndex < rows.length - 1) {
+                    const nextRow = newRows[activeRowIndex + 1];
+
+                    // Recopie les bonnes couleurs trouvées dans la ligne suivante
+                    for (let i = 0; i < currentRow.colors.length; i++) {
+                        if (currentRow.colors[i] === newCode[i]) {
+                            nextRow.colors[i] = currentRow.colors[i];
                         }
                     }
-                } 
-    
-                setRows(newRows);
-                if (exactMatches === 4 || activeRowIndex === 7) {
-                    setGameOver(true);
-                    // Si le code a été trouvé sur la ligne active, passez la ligne active en inactif
-                    if (exactMatches === 4) {
-                        setActiveRowIndex(-1);
+
+                    // Trouve le premier slot vide
+                    const firstEmptySlotIndex = nextRow.colors.findIndex(color => color === '');
+
+                    if (firstEmptySlotIndex !== -1) {
+                        // Si le slot 0 est déjà rempli, passe au premier slot vide
+                        setSelectedPawnIndex(firstEmptySlotIndex);
                     }
+                }
+            }
+
+            setRows(newRows);
+            // Détermine si le jeu est terminé
+            if (exactMatches === 4 || activeRowIndex === 7) {
+                setGameOver(true);
+                if (exactMatches === 4) {
+                    setActiveRowIndex(-1);
+                    handleOpenModal(<div><h2>C'est gagné!</h2><p>Félicitations, vous avez trouvé le code secret!</p></div>);
                 } else {
-                    setActiveRowIndex(activeRowIndex + 1);
+                    handleOpenModal("C'est perdu!");
+                }
+            } else {
+                setActiveRowIndex(activeRowIndex + 1);
+                if (level !== 'Facile') {
                     setSelectedPawnIndex(0);
                 }
             }
         }
-    }, [activeRowIndex, gameOver, rows, checkRow, level, newCode, setRows, setGameOver, setActiveRowIndex, setSelectedPawnIndex]);
+    }
+}, [activeRowIndex, gameOver, rows, checkRow, level, newCode, setRows, setGameOver, setActiveRowIndex, setSelectedPawnIndex]);
+
     
 
+    // Effet pour écouter les touches du clavier
     useEffect(() => {
         const handleKeyDown = (event) => {
+            // Vérifie si la partie n'est pas terminée et qu'une rangée est active
             if (!gameOver && activeRowIndex !== -1) {
                 if (event.key === 'ArrowLeft') {
                     setSelectedPawnIndex((selectedPawnIndex - 1 + 4) % 4);
@@ -139,11 +174,13 @@ export default function Board({ level }) {
             }
         };
 
+        // Fonction pour changer la couleur sélectionnée
         const changeColor = (direction) => {
             const row = rows[activeRowIndex];
             const currentColorIndex = colorsCode.indexOf(row.colors[selectedPawnIndex]);
             let newColorIndex;
 
+            // Calcul du nouvel index de couleur en fonction de la direction
             if (direction === 'down') {
                 if (row.colors[selectedPawnIndex] === '') {
                     newColorIndex = colorsCode.length - 1;
@@ -153,7 +190,7 @@ export default function Board({ level }) {
             } else {
                 newColorIndex = currentColorIndex === colorsCode.length - 1 ? 0 : currentColorIndex + 1;
             }
-            const newColor = colorsCode[newColorIndex]; // Utilisez colorsCode ici
+            const newColor = colorsCode[newColorIndex];
             const newRows = [...rows];
             const newRow = { ...newRows[activeRowIndex] };
             newRow.colors[selectedPawnIndex] = newColor;
@@ -166,23 +203,29 @@ export default function Board({ level }) {
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
         };
-    }, [activeRowIndex, selectedPawnIndex, gameOver, rows, handleCheck, colorsCode]); // Ajoutez colorsCode ici
+    }, [activeRowIndex, selectedPawnIndex, gameOver, rows, handleCheck, colorsCode]);
 
+    // Gestion du clic sur un pion
     const handleSlotClick = (index) => {
+        // Vérifie si la partie n'est pas terminée et qu'une rangée est active
         if (!gameOver && activeRowIndex !== -1) {
             setSelectedPawnIndex(index);
         }
     };
 
+    // Gestion de la sélection de couleur
     const handleColorSelect = (color) => {
+        // Vérifie si la partie n'est pas terminée et qu'une rangée est active
         if (!gameOver && activeRowIndex !== -1) {
             const newRows = [...rows];
             const newRow = { ...newRows[activeRowIndex] };
             newRow.colors[selectedPawnIndex] = color;
 
-            // Trouver l'index du prochain slot vide
+            // Trouver l'index du prochain emplacement vide
             let nextEmptySlotIndex = selectedPawnIndex;
-            let allSlotsFilled = true; // Variable pour vérifier si tous les slots sont remplis
+            let allSlotsFilled = true; 
+
+            // Parcourir la rangée pour trouver le prochain emplacement vide
             for (let i = 0; i < newRow.colors.length; i++) {
                 if (newRow.colors[i] === '') {
                     nextEmptySlotIndex = i;
@@ -191,9 +234,22 @@ export default function Board({ level }) {
                 }
             }
 
-            // Si tous les slots sont remplis, revenir au premier slot
+            // Si tous les slots sont remplis, rechercher le premier pion vide
             if (allSlotsFilled) {
-                nextEmptySlotIndex = 0;
+                for (let i = 0; i < newRow.colors.length; i++) {
+                    if (newRow.colors[i] === '') {
+                        nextEmptySlotIndex = i;
+                        break;
+                    }
+                }
+            } else {
+                // Sélectionner le premier pion vide après le pion actuellement sélectionné
+                for (let i = selectedPawnIndex + 1; i < newRow.colors.length; i++) {
+                    if (newRow.colors[i] === '') {
+                        nextEmptySlotIndex = i;
+                        break;
+                    }
+                }
             }
 
             setSelectedPawnIndex(nextEmptySlotIndex);
@@ -203,51 +259,53 @@ export default function Board({ level }) {
         }
     };
     
+    // Fonction de démarrage du jeu
     const startGame = () => {
         setActiveRowIndex(0);
         setRows(Array.from({ length: 8 }, () => ({ colors: ['', '', '', ''], exactMatches: 0, misplacedMatches: 0, remainingDotsLength: 4 })));
         setGameOver(false);
         handleGenerateCode();
-        console.log("test")
+        setSelectedPawnIndex(0);
     };
 
     return (
         <div className="board">
             <Modal isOpen={modalOpen} onClose={handleCloseModal}>
-                    <div className='modal__rules'>
-                        <span className='modal__title'>Règles du jeu TicTac Boom</span>
-                        <div className='modal__section'>
-                            <p className='modal__text'>TicTac Boom est un jeu de déduction basé sur Mastermind.</p>
-                            <p className='modal__text'>Découvrez le code secret avant la fin du temps imparti pour gagner.</p>
-                        </div>
-                        <div className='modal__section'>
-                            <p className='modal__text'><strong>3 niveaux de jeu</strong> pour toute la famille mais un seul but :</p>
-                            <p className='modal__text'><strong>60 secondes</strong> avant que la bombe n'explose.</p>
-                        </div>
-                        <div className='modal__section'>
-                            <ul className='modal__text'>Avancez ligne par ligne et découvrez votre progression :
-                                <li className='modal__text'><strong>1 pion blanc</strong> = vous avez une bonne couleur mais mal placée</li>
-                                <li className='modal__text'><strong>1 pion noir</strong> = vous avez une bonne couleur et bien placée</li>
-                            </ul>
-                        </div>
-                        <div className='modal__section'>
-                            <p className='modal__text'>Jouez avec votre souris ou directement avec <strong>les touches directionnelles du clavier</strong>.</p>
-                        </div>
-                        <div className='modal__section'>
-                            <p className='modal__text'>Prêt à relever le défi ? À vous de jouer !</p>
-                        </div>
+                <div className='modal__rules'>
+                    <span className='modal__title'>Règles du jeu TicTac Boom</span>
+                    <div className='modal__section'>
+                        <p className='modal__text'>TicTac Boom est un jeu de déduction basé sur Mastermind.</p>
+                        <p className='modal__text'>Découvrez le code secret avant la fin du temps imparti pour gagner.</p>
                     </div>
-                </Modal>
+                    <div className='modal__section'>
+                        <p className='modal__text'><strong>3 niveaux de jeu</strong> pour toute la famille mais un seul but :</p>
+                        <p className='modal__text'><strong>60 secondes</strong> avant que la bombe n'explose.</p>
+                    </div>
+                    <div className='modal__section'>
+                        <ul className='modal__text'>Avancez ligne par ligne et découvrez votre progression :
+                            <li className='modal__text'><strong>1 pion blanc</strong> = vous avez une bonne couleur mais mal placée</li>
+                            <li className='modal__text'><strong>1 pion noir</strong> = vous avez une bonne couleur et bien placée</li>
+                        </ul>
+                    </div>
+                    <div className='modal__section'>
+                        <p className='modal__text'>Jouez avec votre souris ou directement avec <strong>les touches directionnelles du clavier</strong>.</p>
+                    </div>
+                    <div className='modal__section'>
+                        <p className='modal__text'>Prêt à relever le défi ? À vous de jouer !</p>
+                    </div>
+                </div>
+            </Modal>
             <div className='board__header'>
                 <Header/>
-                <ControlsPanel startGame={startGame} handleOpenModal={handleOpenModal}/>
+                <ControlsPanel startGame={startGame} handleOpenModal={handleOpenModal} handleOpenMessage={handleOpenMessage} toggleAnimation={toggleAnimation}/>
             </div>
             <div className='container__secretcode'>
                 <div className={`text__secretcode ${gameOver ? 'animate' : ''}`}>Secret Code</div>
                 {gameOver && <SecretCode newCode={newCode}/>}
             </div>
             <div className='board__game'>
-                <AnimationBomb/>
+                <Pause isOpen={messageOpen} backToGame={handleCloseMessage} restartAnimation={restartAnimation}/>
+                <AnimationBomb isPaused={isPaused} />
                 <p className='game__niveau'>Niveau : {level}</p>
                 <div className='rows'>
                     {rows.map((row, index) => (
@@ -268,6 +326,7 @@ export default function Board({ level }) {
                     onClick={handleColorSelect}
                 />
             </div>
+            <button onClick={toggleAnimation}>test</button>
         </div>
     );
 };
--- src/components/controlspanel/index.jsx
+++ src/components/controlspanel/index.jsx
@@ -4,8 +4,13 @@ import ROUTES from '../../router/routes';
 import './styles.scss';
 import Icon from '../icon';
 
-export default function ControlsPanel ({ startGame, handleOpenModal }) {
-   
+export default function ControlsPanel ({ startGame, handleOpenModal, handleOpenMessage, toggleAnimation }) {
+   // Fonction pour appeler handleOpenMessage et toggleAnimation
+   const handleHandleOpenMessageAndToggleAnimation = () => {
+    toggleAnimation(); // Appelle la fonction toggleAnimation
+    handleOpenMessage(); // Appelle la fonction handleOpenMessage
+    };
+
     return (
         <div className='controlsPanel'>
             <Icon onClick={handleOpenModal}>
@@ -13,7 +18,7 @@ export default function ControlsPanel ({ startGame, handleOpenModal }) {
                     <path d="M80 160c0-35.3 28.7-64 64-64h32c35.3 0 64 28.7 64 64v3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74V320c0 17.7 14.3 32 32 32s32-14.3 32-32v-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7V160c0-70.7-57.3-128-128-128H144C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/>
                 </svg>
             </Icon>
-            <Icon>
+            <Icon onClick={handleHandleOpenMessageAndToggleAnimation}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                     <path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/>
                 </svg>
@@ -34,3 +39,5 @@ export default function ControlsPanel ({ startGame, handleOpenModal }) {
         </div>
     );
 };
+
+
